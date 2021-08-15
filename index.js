72ecf2a5173b1a4badc3176371d095c94e47ba2e
TheMinecrafter05 <59798742+TheMinecrafter05@users.noreply.github.com>
2021-08-16 00:07:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,34 +8,37 @@ class slashCommand{
         this.description = "";
         this.options = []
         this.setName = function(name=""){
-            if(!name) throw new Error("No name provided.");
-            that.name = name
+            that.name = name.toLowerCase()
             return that;
         }
 
         this.setDescription = function(description=""){
-            if(!description) throw new Error("No description provided.");
             that.description = description;
             return that;
         }
 
         this.addOption = function(option=slashOption){
+            if(!option) throw new Error("No option provided.");
             let cmd = {}
-            cmd["name"] = option["name"]
+            cmd["name"] = option["name"].toLowerCase()
             cmd["description"] = option["description"]
             cmd["type"] = option["type"]
             cmd["required"] = option["required"]
+            cmd["choices"] = option["choices"]
             that.options.push(cmd)
         }
 
         this.addOptions = function(options=[]){
+            if(!options.length) throw new Error("Options need to be an object[]");
+            if(options.length == 0) throw new Error("No options provided.");
             for(var i=0;i<options.length;i++){
                 let cmd = {}
                 let option = options[i]
-                cmd["name"] = option["name"]
+                cmd["name"] = option["name"].toLowerCase()
                 cmd["description"] = option["description"]
                 cmd["type"] = option["type"]
                 cmd["required"] = option["required"]
+                cmd["choices"] = option["choices"]
                 that.options.push(cmd)
             }
         }
@@ -45,7 +48,24 @@ class slashCommand{
                 clearInterval(f)
                 let fouund = false;
                 let edit = false;
-                let id
+                let id;
+                if(!that.name) throw new Error("No name provided.");
+                if(that.name.length > 32) throw new Error("Name must be shorter than 32 characters.");
+                if(!that.description) throw new Error("No description provided.");
+                if(that.description.length > 100) throw new Error("Description must be shorter than 100 characters.");
+                for(var i=0;i<this.options.length;i++){
+                    if(this.options[i].name.length > 32) throw new Error("Name must be shorter that 32 characters.");
+                    if(!this.options[i].description) throw new Error("No description provided.");
+                    if(this.options[i].description.length > 100) throw new Error("Description must be shorter than 100 characters.");
+                    if(!this.options[i].type) throw new Error("No type provided.");
+                    if(this.options[i].type == "unvalid"){ throw new Error("Invalid type provided.")}
+                    for(var o=0;o<this.options[i].choices.length;o++){
+                        if(!this.options[i].choices[o].name) throw new Error("No name provided.");
+                        if(!this.options[i].choices[o].value) throw new Error("No value provided.");
+                        if(this.options[i].choices[o].value.toString().length > 100) throw new Error("Value must be shorter than 100 characters.");
+                        if(this.options[i].choices[o].name.length > 100) throw new Error("Name must be shorter than 100 characters.");
+                    }
+                }
                 let hfejhf = await client.api.applications(client.user.id).commands.get();
                 hfejhf.forEach(command=>{
                     if(command.name == that.name){
@@ -67,6 +87,26 @@ class slashCommand{
                                         }
                                         if(command.options[i].type != that.options[i].type){
                                             edit = true;
+                                        }
+                                        if(!command.options[i].choices && that.options[i].choices || command.options[i].choices && !that.options[i].choices){
+                                            edit = true;
+                                        }else{
+                                            if(command.options[i].choices.length != that.options[i].choices.length){
+                                                edit = true;
+                                            }else{
+                                                for(o=0;o<that.options[i].length;o++){
+                                                    if(command.options[i].choices){
+                                                        if(command.options[i].choices[o].name != that.options[i].choices[o].name){
+                                                            edit = true;
+                                                        }
+                                                        if(command.options[i].choices[o].value != that.options[i].choices[o].value){
+                                                            edit = true;
+                                                        }
+                                                    }else{
+                                                        edit = true;
+                                                    }
+                                                }
+                                            }
                                         }
                                     }else{
                                         edit = true;
@@ -117,44 +157,46 @@ class guildSlashCommand{
         this.options = []
         this.guildID = ""
         this.setName = function(name=""){
-            if(!name) throw new Error("No name provided.");
             that.name = name
             return that;
         }
 
         this.setDescription = function(description=""){
-            if(!description) throw new Error("No description provided.");
             that.description = description;
             return that;
         }
 
         this.setGuildID = function(guildID){
-            if(guildID == undefined) throw new Error("No guildID provided.");
             that.guildID = guildID;
             return that;
         }
 
         this.addOption = function(option=slashOption){
+            if(!option) throw new Error("No option provided.");
             let cmd = {}
-            cmd["name"] = option["name"]
+            cmd["name"] = option["name"].toLowerCase()
             cmd["description"] = option["description"]
             cmd["type"] = option["type"]
             cmd["required"] = option["required"]
+            cmd["choices"] = option["choices"]
             that.options.push(cmd)
             return that
         }
 
         this.addOptions = function(options=[]){
+            if(!options.length) throw new Error("Options need to be an object[]");
+            if(options.length == 0) throw new Error("No options provided.");
             for(var i=0;i<options.length;i++){
                 let cmd = {}
                 let option = options[i]
-                cmd["name"] = option["name"]
+                cmd["name"] = option["name"].toLowerCase()
                 cmd["description"] = option["description"]
                 cmd["type"] = option["type"]
                 cmd["required"] = option["required"]
+                cmd["choices"] = option["choices"]
                 that.options.push(cmd)
             }
-            return that
+            return that;
         }
 
         let f = setInterval(async ()=>{
@@ -162,7 +204,27 @@ class guildSlashCommand{
                 clearInterval(f)
                 let fouund = false;
                 let edit = false;
-                let id
+                let id;
+                if(!that.name) throw new Error("No name provided.");
+                if(that.name.length > 32) throw new Error("Name must be shorter than 32 characters.");
+                if(!that.description) throw new Error("No description provided.");
+                if(that.description.length > 100) throw new Error("Description must be shorter than 100 characters.");
+                if(!that.guildID) throw new Error("No guildID provided.");
+                if(!client.guilds.cache.get(that.guildID)) throw new Error("Invalid guildID provided.");
+                if(!this.name) throw new Error("No name provided.");
+                for(var i=0;i<this.options.length;i++){
+                    if(this.options[i].name.length > 32) throw new Error("Name must be shorter that 32 characters.");
+                    if(!this.options[i].description) throw new Error("No description provided.");
+                    if(this.options[i].description.length > 100) throw new Error("Description must be shorter than 100 characters.");
+                    if(!this.options[i].type) throw new Error("No type provided.");
+                    if(this.options[i].type == "unvalid"){ throw new Error("Invalid type provided.")}
+                    for(var o=0;o<this.options[i].choices.length;o++){
+                        if(!this.options[i].choices[o].name) throw new Error("No name provided.");
+                        if(!this.options[i].choices[o].value) throw new Error("No value provided.");
+                        if(this.options[i].choices[o].value.toString().length > 100) throw new Error("Value must be shorter than 100 characters.");
+                        if(this.options[i].choices[o].name.length > 100) throw new Error("Name must be shorter than 100 characters.");
+                    }
+                }
                 let hfejhf = await client.api.applications(client.user.id).guilds(that.guildID).commands.get();
                 hfejhf.forEach(command=>{
                     if(command.name == that.name){
@@ -184,6 +246,26 @@ class guildSlashCommand{
                                         }
                                         if(command.options[i].type != that.options[i].type){
                                             edit = true;
+                                        }
+                                        if(!command.options[i].choices && that.options[i].choices || command.options[i].choices && !that.options[i].choices){
+                                            edit = true;
+                                        }else{
+                                            if(command.options[i].choices.length != that.options[i].choices.length){
+                                                edit = true;
+                                            }else{
+                                                for(o=0;o<that.options[i].choices.length;o++){
+                                                    if(command.options[i].choices[o]){
+                                                        if(command.options[i].choices[o].name != that.options[i].choices[o].name){
+                                                            edit = true;
+                                                        }
+                                                        if(command.options[i].choices[o].value != that.options[i].choices[o].value){
+                                                            edit = true;
+                                                        }
+                                                    }else{
+                                                        edit = true;
+                                                    }
+                                                }
+                                            }
                                         }
                                     }else{
                                         edit = true;
@@ -232,26 +314,28 @@ class slashOption{
         this.description = "";
         this.required = false;
         this.type = 3;
+        this.choices = []
         this.setName = function(name=""){
-            if(!name) throw new Error("No name provided.");
-            that.name = name
+            while(name.includes(" ")){
+                name = name.replace(" ","_")
+            }
+            that.name = name.toLowerCase()
             return that;
         }
 
         this.setDescription = function(description=""){
-            if(!description) throw new Error("No description provided.");
             that.description = description;
             return that;
         }
 
         this.setRequired = function(state = false){
             if(state == undefined) throw new Error("No state provided.");
+            if(typeof(state) != "boolean") throw new Error("Invalid state provided.");
             that.required = state;
             return that;
         }
 
         this.setType = function(type){
-            if(!type){ throw new Error("Invalid type provided.")}
             if(type.toLowerCase() == "string"){ type = 3} else
             if(type.toLowerCase() == "number") {type = 10} else
             if(type.toLowerCase() == "boolean") {type = 5} else
@@ -260,24 +344,61 @@ class slashOption{
             if(type.toLowerCase() == "role") {type = 8} else {
                 type = "unvalid"
             }
-            if(type == "unvalid"){ throw new Error("Invalid type provided.")}
             that.type = type;
+            return that;
+        }
+
+        this.setChoices = function(choices=[]){
+            if(!choices.length) throw new Error("Choices must be an object[].");
+            if(choices.length == 0) throw new Error("No choices provided.");
+            for(var i=0;i<choices.length;i++){
+                that.choices.push(choices[i]);
+            }
             return that;
         }
     }
 }
 
-function onExecute(client=discord.Client, listener = function(command="", interaction, args){}){
+class slashOptionChoice{
+    constructor(){
+        let that = this;
+        this.name = "";
+        this.value = "";
+
+        this.setName = function(name=""){
+            that.name = name;
+            return that;
+        }
+
+        this.setValue = function(value=""){
+            that.value = value;
+            return that;
+        }
+    }
+}
+
+function onExecute(client=discord.Client, listener = function(){}){
     client.ws.on('INTERACTION_CREATE', async interaction => {
         if(!interaction.data.name) return;
-        const command = interaction.data.name.toLowerCase();
-        const args = interaction.data.options;
-        listener(command, interaction, args)
+        let msgObj = {
+            content:interaction.data.name,
+            id:interaction.id,
+            command_id:interaction.data.id,
+            options:interaction.data.options,
+            channel:client.channels.cache.get(interaction.channel_id),
+            guild: client.guilds.cache.get(interaction.guild_id),
+            author: interaction.member.user,
+            member: interaction.member,
+            application_id:interaction.application_id,
+            interaction: interaction,
+            client:client
+        }
+        listener(msgObj)
     });
 }
 
-async function reply(client, interaction,text, private=false){
-   await client.api.interactions(interaction.id, interaction.token).callback.post({
+async function reply(message,text, private=false){
+   await message.client.api.interactions(message.interaction.id, message.interaction.token).callback.post({
         data: {
             type: 5,
             data:{
@@ -287,22 +408,13 @@ async function reply(client, interaction,text, private=false){
     }).catch(err=>{
         if(err) {console.warn(err); return undefined;}
     })
-    return await new discord.WebhookClient(client.user.id,interaction.token).send(typeof(text) == "object" ? {embeds:[text]} : text)
+    let f = await new discord.WebhookClient(message.client.user.id,message.interaction.token).send(typeof(text) == "object" ? {embeds:[text]} : text)
+    let r = await message.channel.messages.fetch({limit: 15})
+    r = r.get(f.id);
+    return r ? r : undefined;
 }
 
-async function remove(client, interaction){
-    await client.api.webhooks(client.user.id, interaction.token).messages('@original').delete().catch(e=>null)
-}
-
-async function edit(client, message, interaction){
-    if(typeof(message) == "object"){
-        await client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({data: {content:"",embeds:[message]}}).catch(e=>console.error)
-    }else{
-        await client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({data: {content:message, embeds:[]}}).catch(e=>console.error)
-    }
-}
-
-async function deleteslashCommand(client, cmd=""){
+async function deleteSlashCommand(client, cmd=""){
     let f = setInterval(async ()=>{
         if(client.readyAt != null){
             clearInterval(f)
@@ -330,4 +442,4 @@ async function deleteGuildSlashCommand(client, cmd="", guildId=""){
     },100)
 }
 
-module.exports = {slashCommand,guildSlashCommand,slashOption, onExecute, reply, remove,edit, deleteslashCommand, deleteGuildSlashCommand}
+module.exports = {slashCommand,guildSlashCommand,slashOption,slashOptionChoice, onExecute, reply, deleteSlashCommand, deleteGuildSlashCommand}
